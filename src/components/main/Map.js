d57f0b5c7046bef3586cf8c7f485764fc4613995
saebom <boma91@gmail.com>
2021-10-16 01:29:30 +0900

    지도 타입별 버튼에 토글 기능 적용 완료

--- src/components/main/Map.js
+++ src/components/main/Map.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react/cjs/react.development';
 import styled from 'styled-components';
-import { DownCircleFilled } from '@ant-design/icons';
-import { Button, Modal } from 'antd';
+
 
 const StyledMapContainer = styled.div`
     width: 1000px;
@@ -15,130 +15,52 @@ const StyledMap = styled.div`
   height:100%;
 `;
 
-const StyledMoreOption = styled(DownCircleFilled)`
-  font-size: 35px;
-  color: coral;
-  transition: transform 300ms ease;
-  :hover {
-    transform: scale(1.1);
-  }
-`;
-
-const StyledModal = styled(Modal)`
-  max-width: 400px;
-  .ant-modal-body {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    width: 400px;
-    height: 150px;
-    margin: auto;
-    padding-top: 50px;
-  }
-  img {
-    width: 45px !important;
-    height: 45px;
-    border-radius: 8px;
-    cursor: pointer;
-    transition: transform 300ms ease;
-    :hover {
-      transform: scale(1.1);
-    }
-  }
-  #trafficBtn, #loadBtn, #bicycleBtn {
-    margin-right: 3%;
-    background-color: coral;
-    color: #fff;
-    border: none;
-    border-radius: 5px;
-    box-shadow: 2px 2px 2px 2px #E1E2E1;
-    transition: transform 300ms ease;
-    :hover {
-      transform: scale(1.1);
-    }
-  }
-`;
-
 const { kakao } = window;
 
 const Map = ({ myRide }) => {
-    const [isModalVisible, setIsModalVisible] = useState(false);
-    const [optionBtnToggle, setOptionBtnToggle] = useState(false);
+  const [placeId, setPlaceId] = useState([])
 
-    const showModal = () => {
-        setIsModalVisible(true);
-    };
-
-    const handleOk = () => {
-        setIsModalVisible(false);
-        setOptionBtnToggle(!optionBtnToggle)
-      };
-    
-      const handleOk1 = () => {
-        setIsModalVisible(false);
-       // map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
-      };
-    
-      const handleOk2 = () => {
-        setIsModalVisible(false);
-        //map.addOverlayMapTypeId(kakao.maps.MapTypeId.ROADVIEW);
-      };
       useEffect(() => {
         let infowindow = new kakao.maps.InfoWindow({zIndex:1});
         const container = document.getElementById('map');
-		const options = {
-			center: new kakao.maps.LatLng(35.157588, 129.058822),
-			level: 2,
-		};
+        const options = {
+          center: new kakao.maps.LatLng(37.566826, 126.9786567),
+          level: 3,
+        };
         const map = new kakao.maps.Map(container, options);
-    	const ps = new kakao.maps.services.Places(); 
-        ps.keywordSearch(myRide, placesSearchCB); 
+        const ps = new kakao.maps.services.Places(); 
+          ps.keywordSearch(myRide, placesSearchCB); 
 
-        function placesSearchCB (data, status) {
+        function placesSearchCB(data, status) {
+          console.log('data : ', data )
             if (status === kakao.maps.services.Status.OK) {
                 let bounds = new kakao.maps.LatLngBounds();
                 for (let i=0; i<data.length; i++) {
                     displayMarker(data[i]);    
                     bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
                 }       
-                map.setBounds(bounds);
+              map.setBounds(bounds);
+              setPlaceId(data[0].id)
             } 
         }
+        console.log('placeId', placeId)
         function displayMarker(place) {
             let marker = new kakao.maps.Marker({
                 map: map,
                 position: new kakao.maps.LatLng(place.y, place.x) 
             })
-            
             kakao.maps.event.addListener(marker, 'click', function() {
                 infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
                 infowindow.open(map, marker);
             });
-        }  
-
-    
-    //props.optionBtnToggle ? map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC) : map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+        }
+        
+        //myRide.optionBtnToggle ? map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC) : map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+     
     }, [myRide]);
 
     return (
         <>
-            <StyledMoreOption onClick={showModal} /> 
- 
-            <StyledModal visible={isModalVisible} onCancel={handleOk} footer={null} closable={true} >
-                <Button id="trafficBtn" type="primary" onClick={handleOk1}>교통정보</Button>
-                <Button id="loadBtn" type="primary" onClick={handleOk2}>로드뷰</Button>
-                <Button id="bicycleBtn" type="primary" onClick={() => {
-                handleOk();
-                // map.addOverlayMapTypeId(kakao.maps.MapTypeId.BICYCLE);
-                }}>자전거</Button>
-                <img src={require("../../img/kakao_navi.png").default}
-                onClick={() => {
-                    handleOk();
-                    window.open('https://map.kakao.com/link/to/18577297');
-                    }}
-                style={{ width: "30px" }}
-                />
-            </StyledModal> 
                         
             <StyledMapContainer>
                 <StyledMap id='map'/>
--- src/components/main/MyRide.js
+++ src/components/main/MyRide.js
@@ -1,10 +1,9 @@
 /*global kakao */
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
-import { Button } from 'antd';
-import { SearchOutlined } from '@ant-design/icons';
+import { Button, Modal } from 'antd';
+import { SearchOutlined, DownCircleFilled } from '@ant-design/icons';
 import 'antd/dist/antd.css'
-import Map from './Map';
 
 const StyledInputContainer = styled.div`
   margin: 1.5%  0;
@@ -39,19 +38,171 @@ const StyledInputContainer = styled.div`
   }
 `;
 
+const StyledMoreOption = styled(DownCircleFilled)`
+  font-size: 35px;
+  color: coral;
+  transition: transform 300ms ease;
+  :hover {
+    transform: scale(1.1);
+  }
+`;
+
+const StyledModal = styled(Modal)`
+  max-width: 400px;
+  .ant-modal-body {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 400px;
+    height: 150px;
+    margin: auto;
+    padding-top: 50px;
+  }
+  img {
+    width: 45px !important;
+    height: 45px;
+    border-radius: 8px;
+    cursor: pointer;
+    transition: transform 300ms ease;
+    :hover {
+      transform: scale(1.1);
+    }
+  }
+  #trafficBtn, #loadBtn, #bicycleBtn {
+    margin-right: 3%;
+    color: coral;
+    border: none;
+    border-radius: 5px;
+    box-shadow: 2px 2px 2px 2px #E1E2E1;
+    transition: transform 300ms ease;
+    :hover {
+      transform: scale(1.1);
+    }
+  }
+`;
+
+const StyledMapContainer = styled.div`
+    width: 1000px;
+    height: 80%;
+    border: 1px solid #A5D6A7;
+    border-radius: 8px;
+`;
+
+const StyledMap = styled.div`
+  width:100%;
+  height:100%;
+`;
+
+const { kakao } = window;
 const MyRide = () => {
   const [inputText, setInputText] = useState("");
   const [place, setPlace] = useState("");
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [placeId, setPlaceId] = useState("");
+
+  const [trafficBtnToggle, setTrafficBtnToggle] = useState(false);
+  const [loadBtnToggle, setLoadBtnToggle] = useState(false);
+  const [bicycleBtnToggle, setBicycleBtnToggle] = useState(false);
 
   const searchInputOnChange = (e) => {
     setInputText(e.target.value);
   }
 
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setPlace(inputText);
     setInputText("");
-  }; 
+  };
+
+  
+ useEffect(() => {
+  let infowindow = new kakao.maps.InfoWindow({zIndex:1});
+  const container = document.getElementById('map');
+  const options = {
+    center: new kakao.maps.LatLng(37.566826, 126.9786567),
+    level: 3,
+  };
+  const map = new kakao.maps.Map(container, options);
+  const ps = new kakao.maps.services.Places(); 
+    ps.keywordSearch(place, placesSearchCB); 
+
+  function placesSearchCB(data, status) {
+    console.log('data : ', data )
+      if (status === kakao.maps.services.Status.OK) {
+          let bounds = new kakao.maps.LatLngBounds();
+          for (let i=0; i<data.length; i++) {
+              displayMarker(data[i]);    
+              bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+          }       
+        map.setBounds(bounds);
+        setPlaceId(data[0].id)
+      } 
+  }
+  console.log('placeId', placeId)
+  function displayMarker(place) {
+      let marker = new kakao.maps.Marker({
+          map: map,
+          position: new kakao.maps.LatLng(place.y, place.x) 
+      })
+      kakao.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+          infowindow.open(map, marker);
+      });
+   }
+   
+   trafficBtnToggle ? map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC) : map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+   loadBtnToggle ? map.addOverlayMapTypeId(kakao.maps.MapTypeId.ROADVIEW) : map.removeOverlayMapTypeId(kakao.maps.MapTypeId.ROADVIEW);    
+   bicycleBtnToggle ? map.addOverlayMapTypeId(kakao.maps.MapTypeId.BICYCLE) : map.removeOverlayMapTypeId(kakao.maps.MapTypeId.BICYCLE);
+   
+}, [place, trafficBtnToggle, loadBtnToggle, bicycleBtnToggle]);
+
+  
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleTrafficOption = () => {
+    const trafficBtn = document.getElementById('trafficBtn').style;
+    setTrafficBtnToggle(!trafficBtnToggle);
+    if (trafficBtnToggle) {
+      trafficBtn.backgroundColor = 'white'
+      trafficBtn.color = 'coral'
+    }
+    else {
+      trafficBtn.backgroundColor = 'coral'
+      trafficBtn.color = 'white'
+    }
+  };
+
+  const handleLoadOption = () => {
+    const loadBtn = document.getElementById('loadBtn').style;
+    setLoadBtnToggle(!loadBtnToggle);
+    if (!loadBtnToggle) {
+      loadBtn.backgroundColor = 'coral'
+      loadBtn.color = 'white'
+    }
+    else {
+      loadBtn.backgroundColor = 'white'
+      loadBtn.color = 'coral'
+    }
+  }
+
+  const handleBicycleOption = () => {
+    const bicycleBtn = document.getElementById('bicycleBtn').style;
+    setBicycleBtnToggle(!bicycleBtnToggle);
+    if (!bicycleBtnToggle) {
+      bicycleBtn.backgroundColor = 'coral'
+      bicycleBtn.color = 'white'
+    }
+   else {
+      bicycleBtn.backgroundColor = 'white'
+      bicycleBtn.color = 'coral'
+    }
+  }
 
   return (
     <div class="MyRide">
@@ -61,13 +212,30 @@ const MyRide = () => {
         <form onSubmit={handleSubmit}>
             <div class="InputContainer">
               <input value={ inputText } onChange={searchInputOnChange} id="keyword"  type="text" placeholder="위치 검색하기" />
-              <Button id="search" htmlType="submit"  icon={<SearchOutlined />}> Search </Button>  
+              <Button id="search" htmlType="submit" icon={<SearchOutlined />}> Search </Button>
             </div>
-            
+            <StyledMoreOption onClick={showModal} /> 
           </form>
       </StyledInputContainer>
+ 
+      <StyledModal visible={isModalVisible} onCancel={handleOk} footer={null} closable={true} title="지도 타입 변경하기">
+          <Button id="trafficBtn" onClick={handleTrafficOption}>교통정보</Button>
+          <Button id="loadBtn" onClick={handleLoadOption}>로드뷰</Button>
+          <Button id="bicycleBtn" onClick={handleBicycleOption}>자전거</Button>
+
+
+          <img src={require("../../img/kakao_navi.png").default}
+          onClick={() => {
+              handleOk();
+              window.open(`https://map.kakao.com/link/to/${placeId}`);
+              }}
+          style={{ width: "30px" }}
+          />
+      </StyledModal> 
             
-      <Map myRide={place, optionBtnToggle} />
+      <StyledMapContainer>
+                <StyledMap id='map'/>
+            </StyledMapContainer> 
      
 
     </div>
