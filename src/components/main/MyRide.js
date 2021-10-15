fbff40ae3c2a8016c3a949c4405551e2d0f22271
saebom <boma91@gmail.com>
2021-10-15 22:22:00 +0900

    map component로 STATE값 이동

--- src/components/main/MyRide.js
+++ src/components/main/MyRide.js
@@ -1,8 +1,8 @@
 /*global kakao */
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Button, Modal } from 'antd';
-import { SearchOutlined, DownCircleFilled } from '@ant-design/icons';
+import { Button } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
 import 'antd/dist/antd.css'
 import Map from './Map';
 
@@ -39,54 +39,9 @@ const StyledInputContainer = styled.div`
   }
 `;
 
-const StyledMoreOption = styled(DownCircleFilled)`
-  font-size: 35px;
-  color: coral;
-  transition: transform 300ms ease;
-  :hover {
-    transform: scale(1.1);
-  }
-`;
-
-const StyledModal = styled(Modal)`
-  max-width: 400px;
-  .ant-modal-body {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    width: 400px;
-    height: 150px;
-    margin: auto;
-    padding-top: 50px;
-  }
-  img {
-    width: 45px !important;
-    height: 45px;
-    border-radius: 8px;
-    cursor: pointer;
-    transition: transform 300ms ease;
-    :hover {
-      transform: scale(1.1);
-    }
-  }
-  #trafficBtn, #loadBtn, #bicycleBtn {
-    margin-right: 3%;
-    background-color: coral;
-    color: #fff;
-    border: none;
-    border-radius: 5px;
-    box-shadow: 2px 2px 2px 2px #E1E2E1;
-    transition: transform 300ms ease;
-    :hover {
-      transform: scale(1.1);
-    }
-  }
-`;
-
 const MyRide = () => {
   const [inputText, setInputText] = useState("");
   const [place, setPlace] = useState("");
-  const [isModalVisible, setIsModalVisible] = useState(false);
 
   const searchInputOnChange = (e) => {
     setInputText(e.target.value);
@@ -96,30 +51,7 @@ const MyRide = () => {
     e.preventDefault();
     setPlace(inputText);
     setInputText("");
-  };
-
-  const showModal = () => {
-    setIsModalVisible(true);
-  };
-
-  const handleOk = () => {
-    setIsModalVisible(false);
-  };
-
-  const handleOk1 = () => {
-    setIsModalVisible(false);
-   // map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
-  };
-
-  const handleOk2 = () => {
-    setIsModalVisible(false);
-    //map.addOverlayMapTypeId(kakao.maps.MapTypeId.ROADVIEW);
-  };
-
-  const handleOk3 = () => {
-    setIsModalVisible(false);
-    //map.addOverlayMapTypeId(kakao.maps.MapTypeId.BICYCLE);
-  };
+  }; 
 
   return (
     <div class="MyRide">
@@ -128,29 +60,14 @@ const MyRide = () => {
       <StyledInputContainer>
         <form onSubmit={handleSubmit}>
             <div class="InputContainer">
-                <input value={ inputText } onChange={searchInputOnChange} id="keyword"  type="text" placeholder="위치 검색하기" />
-            <Button id="search" htmlType="submit"  icon={<SearchOutlined />}> Search </Button>  
-            {/* <button type="submit">검색</button> */}
+              <input value={ inputText } onChange={searchInputOnChange} id="keyword"  type="text" placeholder="위치 검색하기" />
+              <Button id="search" htmlType="submit"  icon={<SearchOutlined />}> Search </Button>  
             </div>
-            <StyledMoreOption onClick={showModal} />  
+            
           </form>
       </StyledInputContainer>
-      
-      {/* modal */}
-      <StyledModal visible={isModalVisible} onCancel={handleOk} footer={null} closable={true} >
-        <Button id="trafficBtn" type="primary" onClick={handleOk1}>교통정보</Button>
-        <Button id="loadBtn" type="primary" onClick={handleOk2}>로드뷰</Button>
-        <Button id="bicycleBtn" type="primary" onClick={handleOk3}>자전거</Button>
-        <img src={require("../../img/kakao_navi.png").default}
-          onClick={() => {
-            handleOk();
-            window.open('https://map.kakao.com/link/to/18577297');
-            }}
-          style={{ width: "30px" }}
-        />
-      </StyledModal> 
             
-      <Map myRide={place} />
+      <Map myRide={place, optionBtnToggle} />
      
 
     </div>
