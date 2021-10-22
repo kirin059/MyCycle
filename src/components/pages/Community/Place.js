adb0f2fa283e12e9e90ccf8d1c90eb031a5f6674
saebom <boma91@gmail.com>
2021-10-22 21:05:01 +0900

    글 업로드 양식 레이아웃 구현 완료

--- src/components/pages/Community/Place.js
+++ src/components/pages/Community/Place.js
@@ -1,11 +1,60 @@
 import React from 'react';
+import Comment from './Comments'
+import { Input, Form, Button, Descriptions } from 'antd';
+import styled from 'styled-components';
+import moment from 'moment';
+
+const StyledMainContainer = styled(Form.Item)`
+    margin:  0;
+`;
+
+const StyledCommentContainer =  styled(Form.Item)`
+margin:  10px 0;
+`;
 
 const Place = () => {
+
+    const [form] = Form.useForm();
+    const month = moment().format("YYYY-MM-DD")
+
+
+    const onFinish = async () => {
+      try {
+        const values = await form.validateFields();
+        console.log("Submit:", values);
+      } catch (errInfo) {
+        console.log("Error:", errInfo);
+      }
+    };
+    
     return (
         <>
-            <p>this is test</p>
+            <Form form={form} layout="horizontal" onFinish={onFinish} style={{backgroundColor: "#fff"}}>
+                <StyledMainContainer>
+                    <Descriptions bordered>
+                        <Descriptions.Item label="날짜" span={2}>{month}</Descriptions.Item>
+                
+                        <Descriptions.Item label="총 게시글" > 반복문index </Descriptions.Item>
+
+                        <Descriptions.Item style={{ height: "60vh" }} colSpan={3}>
+                            {
+                                <Comment />
+                            }
+                        </Descriptions.Item>
+                    </Descriptions>
+                </StyledMainContainer>
+
+                <StyledCommentContainer
+                    style={{display:"flex"}}
+                    rules={[ {required: true} ]}
+                >
+                    <Input placeholder="You can write to share..." style={{width: "70%", marginRight:"15px"}}/>
+                    <Button htmlType="submit" type="primary"> Comment </Button>
+                </StyledCommentContainer>
+            </Form>
         </>
     );
 };
 
 export default Place;
+
